73856bb6b9768b75d29bfb4b23c95daf63d77a99
Hbrown16 <hbrown1094@gmail.com>
2021-08-04 14:12:42 -0400

    li added to engineer

--- page-template.js
+++ page-template.js
@@ -10,7 +10,7 @@ const generateTeam = team => {
         </div>    
         <div class = "Card-Body">
             <ul class = "List-Group">
-                <li class = "List-Groupe-Item">ID: ${Manager.getId()}</li>
+                <li class = "List-Group-Item">ID: ${Manager.getId()}</li>
                 <li class = "List-Group_Item">Email: <a href="mailto:${getEmail()}">${Manager.getEmail()}</a></li>
                 <li class = "List-Group_Item">Office Number: ${Manager.getOfficeNumber()}</li>
             </ul>
@@ -25,11 +25,14 @@ const generateTeam = team => {
         return`
         <div class = "card employee-card">
     <div class = "Card-Header">
-        <h2 class = "Card-Title">${engineer.getName()}</h2>
-        <h3 class = "Card-Title"><i class = "fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h2 class = "Card-Title">${Engineer.getName()}</h2>
+        <h3 class = "Card-Title"><i class = "fas fa-glasses mr-2"></i>${Engineer.getRole()}</h3>
     </div>
     <div class = "Card-Body">
-    
+        <ul class = "List-Group">
+        <li class = "List-Group-Item">ID: ${Engineer.getId()}</li>
+        <li class = "List-Group-Item">Email: <a href="mailto:${Engineer.getEmail()}">${Engineer.getEmail()}</a></li>
+
     `;    
     }
 }
