1a4b355ddead16d5a25933aa7891c83936dd2f0e
Hbrown16 <hbrown1094@gmail.com>
2021-08-04 13:44:03 -0400

    mangerhtml

---
+++ page-template.js
@@ -0,0 +1,12 @@
+const generateTeam = team => {
+
+    // manager html
+    const generateManger = Manager => {
+        return
+        <div class = "card employee-card">
+        <div class = "card-header">
+            
+        </div>    
+        </div>
+    }
+}
