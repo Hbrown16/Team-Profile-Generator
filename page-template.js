9a793682245f81f4861a5efa2a48799880944526
Hbrown16 <hbrown1094@gmail.com>
2021-08-04 13:48:44 -0400

    manger html 1

--- page-template.js
+++ page-template.js
@@ -4,9 +4,10 @@ const generateTeam = team => {
     const generateManger = Manager => {
         return
         <div class = "card employee-card">
-        <div class = "card-header">
-            
+        <div class = "Card-Header">
+           <h2 class = "Card-Title">${Manager.getName()}</h2>
+           <h3 class = "Card-Title"><i class = class = "fas fa-mug-hot mr-2"></i>${Manager.getRole()}</h3> 
         </div>    
-        </div>
+        <div class
     }
 }
