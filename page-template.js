4e7c0ac88b8c500b14962f7f83f547831135f542
Hbrown16 <hbrown1094@gmail.com>
2021-08-05 19:17:49 -0400

    export 1

--- page-template.js
+++ page-template.js
@@ -91,7 +91,9 @@ module.exports = team => {
     return `
     <!DOCTYPE html>
 <html lang="en">
-    <
+    <meta charset="UTF-8" />
+    <meta name = "viewport" content"width=device-width, initial-scale=1.0" />
+    <meta
 </html>
     `;
 }
