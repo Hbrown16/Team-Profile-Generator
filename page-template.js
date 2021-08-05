1be996633162250543afab3950258611bfbe7852
Hbrown16 <hbrown1094@gmail.com>
2021-08-05 19:19:11 -0400

    exporting

--- page-template.js
+++ page-template.js
@@ -93,7 +93,7 @@ module.exports = team => {
 <html lang="en">
     <meta charset="UTF-8" />
     <meta name = "viewport" content"width=device-width, initial-scale=1.0" />
-    <meta
+    <meta http-equiv = "X-UA-Compatible" content="ie=edge" />
 </html>
     `;
 }
