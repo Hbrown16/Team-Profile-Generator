bf59656c8efdf7c067835c9557851016aadb1f21
Hbrown16 <hbrown1094@gmail.com>
2021-08-21 11:57:11 -0400

    manager info

--- team/Manager.js
+++ team/Manager.js
@@ -1,1 +1,8 @@
 const Employee = require("./Employee");
+
+class Manager extends Employees {
+    constructor (name, id, email, officeNumber){
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+}
