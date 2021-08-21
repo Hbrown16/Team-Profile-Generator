bd78e156e42ef72ee78eee0fbe302bc5acf66998
Hbrown16 <hbrown1094@gmail.com>
2021-08-21 11:43:33 -0400

    engineer js role

--- team/Engineer.js
+++ team/Engineer.js
@@ -3,5 +3,11 @@ const Employee = require("./Employee");
 class Engineer extends Employee {
     constructor(name, id, email, github){
         super(name,id, email);
+        this.gitHub = gitHub;
+    }
+
+    get Role() {
+        return "Engineer";
+        
     }
 }
